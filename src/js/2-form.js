fca00924ae8fe42b074867de8c6a6420ad5dc8fe
OlexandrVakulin <vlipoq@gmail.com>
2025-07-06 20:03:32 +0200

    retake_hw-t2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,33 +1,42 @@
-const formData = {
-  email: " ",
-  message: " "
+let formData = {
+  email: "",
+  message: ""
 }
 const form = document.querySelector(".feedback-form");
 const localStorageKey = "feedback-form-state";
 
-const savedData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+const savedData = JSON.parse(localStorage.getItem(localStorageKey)) || formData;
+console.log(savedData);
 
-form.elements.email.value = savedData.email || "";
-form.elements.message.value = savedData.message || "";
-
-form.addEventListener("input", (event) => {  
+form.addEventListener("input", (event) => {
   const formIn = event.target;
   
   if (formIn.name) {
     formData[formIn.name] = formIn.value.trim();
-  } 
-  localStorage.setItem(localStorageKey, JSON.stringify(formData));  
+  }
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
+
+form.elements.email.value = savedData.email || "";
+form.elements.message.value = savedData.message || "";
+
 
 form.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const textField = evt.target.elements;
 
-  if (textField.email.value === "" || textField.message.value === "") {
+  const currentData = {
+    email: textField.email.value.trim(),
+    message: textField.message.value.trim()
+  };
+
+  if (currentData.email === "" || currentData.message === "") {
     return alert('Fill please all fields');
   }
-  console.log(formData);  
+  console.log(currentData);
   localStorage.removeItem(localStorageKey);
   form.reset();
-  
+  formData.email = "";
+  formData.message = "";
+    
 });
